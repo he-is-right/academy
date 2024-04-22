bedbaed9810d8b283301be5f116c02eb66f5cd9f
Timothy Dawadakpoye <timdawadakpoye@gmail.com>
2024-04-22 10:50:40 +0100

    fixed remult issue

--- src/app/api/[...remult]/route.ts
+++
@@ -1,7 +0,0 @@
-import { remultNextApp } from "remult/remult-next";
-import { Student } from "@/models/student";
-
-const api = remultNextApp({
-    entities: [Student],
-})
-export const { GET, POST, PUT, DELETE } = api
